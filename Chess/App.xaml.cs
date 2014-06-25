982492eaf099e4bf109c703d99a6c2e6653b3c3e
TomHulme <thul006@aucklanduni.ac.nz>
2014-06-25 20:03:30 +1200

    Complete Overhaul
    Fixed a bunch of issues that were messing with making everything not
    work. Renamed compile unit, and project to "Chess".
    
    NOTE: I figured it would be best to do this while John was studying for
    his final exam, and I had time off. I didn't just change everything
    randomly.

---
+++ Chess/App.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Chess
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //public MainMenu mainmenu = new MainMenu();
+        //public Game game = new Game();
+    }
+}
